b1a93cae027b3fa7feb15346dae65f5535ed8931
Boikano Success Ranape <159779806+Icarus9Maximus@users.noreply.github.com>
2024-11-26 10:28:50 +0200

    Update page.jsx

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -8,7 +8,8 @@ import Form from "@components/Form";
 const EditPrompt = () => {
     const router = useRouter();
     // const searchParams = useSearchParams();
-    const promptId = searchParams.get("id");
+    // const promptId = searchParams.get("id");
+    const promptid = 1;
 
     const [submitting, setIsSubmitting] = useState(false);
     const [post, setPost] = useState({
